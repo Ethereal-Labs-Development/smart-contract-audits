5df1102eb6ae559a34e66b8f3e4c13966161ecd0
Andrew Gucci <athoma67@asu.edu>
2023-04-10 13:02:30 -0700

    Set solc version and added sample test.

--- workspaces/forge/test/Dojo.t.sol
+++ workspaces/forge/test/Dojo.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.9;
 
 import "../lib/forge-std/src/Test.sol";
 import "../src/projects/dojo/Dojo.sol";
@@ -10,4 +10,10 @@ contract DojoTest is Test {
     function setUp() public {
         dojo = new DojoCHIP();
     }
+
+    /// @notice Sample test to log the variable's value.
+    function test_deadAddress_Value() public {
+        emit log_named_address("Dead Address", dojo.deadAddress());
+    }
+
 }
